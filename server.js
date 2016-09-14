14eaf5fb538fe5cb8c31bf8671d5dd7b30aefe80
Simon Donaldson <simondonaldson@gmail.com>
2016-09-14 23:58:42 +0100

    domain issues

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.use(function(req, res, next) {
 
     res.header('Build', package.version)
 
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+
     next()
 })
 
