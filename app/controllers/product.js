72495cfdcea7b6f10ed2d92117598b6ff84bf746
Simon Donaldson <simondonaldson@gmail.com>
2016-09-15 11:28:07 +0100

    werk

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -6,20 +6,38 @@ module.exports = {
 
         if (!req.body.user_id || !req.body.name || !req.body.nsfw || !req.body.trend) return next(new Error('Missing user id or product name, trend, nsfw'))
 
-        const basket = new Basket({
-            user_id: req.body.user_id
-        });
 
-        basket.products.push({
-            name: req.body.name,
-            nsfw: req.body.nsfw,
-            trend: req.body.trend
-        });
+        Basket.findOne({ user_id: req.body.user_id }).exec(function(err, basket) {
+            if (err) return next(err)
+            if (basket) {
 
-        basket.save(function(err, basket) {
-            //console.log(err, basket);
-            res.send('ok');
+                basket.products.push({
+                    name: req.body.name,
+                    nsfw: req.body.nsfw,
+                    trend: req.body.trend
+                });
+
+                basket.save(function(err) {
+                    res.json({ success: true });
+                });
+
+            } else {
+                const basket = new Basket({
+                    user_id: req.body.user_id
+                });
+
+                basket.products.push({
+                    name: req.body.name,
+                    nsfw: req.body.nsfw,
+                    trend: req.body.trend
+                });
+
+                basket.save(function(err, basket) {
+                    res.json({ success: true });
+                })
+            }
         })
+
 
     }
 
